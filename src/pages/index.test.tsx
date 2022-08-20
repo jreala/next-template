20669f373252876f39aeada14e4b502c867cd602
Jason Eala <11766330+jreala@users.noreply.github.com>
2022-08-19 21:31:31 -0700

    remove vitest because im tired of fighting next build issues

--- src/pages/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { expect, test} from 'vitest';
-import {render, screen, within} from '@testing-library/react';
-import Home from '~/pages/index';
-
-test('home', () => {
-    render(<Home />);
-    const main = within(screen.getByRole('main'));
-    expect(main).toBeDefined();
-});
